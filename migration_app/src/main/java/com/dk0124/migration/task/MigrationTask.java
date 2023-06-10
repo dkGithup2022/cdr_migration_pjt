67ffc044215262b9b94c0eacf42a39b3a114e490
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2023-06-11 01:02:18 +0900

    migration task 작성 & 실행
    1. bithumb 기준 실행 함수

--- migration_app/src/main/java/com/dk0124/migration/task/MigrationTask.java
+++ migration_app/src/main/java/com/dk0124/migration/task/MigrationTask.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import com.dk0124.cdr.constants.coinCode.bithumbCoinCode.BithumbCoinCode;
 import com.dk0124.cdr.persistence.repositoryUtils.bithumb.candle.BithumbCandleRepositoryUtils;
 import com.dk0124.migration.migration.candles.BithumbCandleMigration;
+import com.dk0124.migration.migration.ticks.BithumbTickMigration;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,16 +18,37 @@ import lombok.extern.slf4j.Slf4j;
 public class MigrationTask implements ApplicationRunner {
 
 	private final BithumbCandleMigration BCMigration;
-	private final BithumbCandleRepositoryUtils repoPicker;
+	private final BithumbTickMigration bithumbTickMigration;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 
+		log.info("DO  : " + System.currentTimeMillis());
+		log.info("\n***********RUN BITHUMB CANDLE***********");
+		run_bithumb_candle();
+
+		log.info("\n***********RUN BITHUMB TICK***********");
+		run_bithumb_tick();
+
+		log.info("DONE  : " + System.currentTimeMillis());
+
+	}
+
+	private void run_bithumb_candle() {
 		for (BithumbCoinCode code : BithumbCoinCode.values()) {
 			log.info(" migrate : {} ", code.toString());
 			BCMigration.setPGRepo(code);
-			BCMigration.readyMigrate(code, 500);
+			BCMigration.readyMigrate(code, 2000);
 			BCMigration.migrate(code);
+		}
+	}
+
+	private void run_bithumb_tick() {
+		for (BithumbCoinCode code : BithumbCoinCode.values()) {
+			log.info(" migrate : {} ", code.toString());
+			bithumbTickMigration.setPGRepo(code);
+			bithumbTickMigration.readyMigrate(code, 2000);
+			bithumbTickMigration.migrate(code);
 		}
 	}
 }
